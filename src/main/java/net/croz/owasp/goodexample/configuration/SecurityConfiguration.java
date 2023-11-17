a863c2679d42d6f555b6f40d75bab656b3bc568f
bbes <bbes@croz.net>
2023-11-17 16:52:51 +0100

    Fixed security

--- src/main/java/net/croz/owasp/goodexample/configuration/SecurityConfiguration.java
+++ src/main/java/net/croz/owasp/goodexample/configuration/SecurityConfiguration.java
@@ -2,18 +2,22 @@ package net.croz.owasp.goodexample.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter.Directive.COOKIES;
 
 @Configuration
-public class SecurityConfiguration {
+public class SecurityConfiguration implements WebMvcConfigurer {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -24,15 +28,17 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             //TODO: ovaj headers je za h2-konzolu
+            .cors()
+            .and()
             .headers().frameOptions().disable()
             .and()
             .csrf()
                 .ignoringAntMatchers("/auth/login", "/auth/password-reset", "/h2-console/**", "/product/**")
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .csrfTokenRepository(getCsrfTokenRepository())
             .and()
             .formLogin()
                 .loginProcessingUrl("/auth/login")
-                .defaultSuccessUrl("/homepage.html", true)
+                .successHandler((req, res, auth) -> res.setStatus(HttpStatus.OK.value()))
                 .failureUrl("/index.html?error=true")
             .and()
             .authorizeRequests()
@@ -49,5 +55,24 @@ public class SecurityConfiguration {
 
         return http.build();
     }
+
+    private CsrfTokenRepository getCsrfTokenRepository() {
+        CookieCsrfTokenRepository tokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse();
+        tokenRepository.setCookiePath("/");
+        tokenRepository.setCookieDomain("owasp-guidelines-good.com");
+        return tokenRepository;
+    }
+
+    //TODO: ispravi ovaj CORS
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://owasp-guidelines-good.com")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("Authorization", "Content-Type", "X-Requested-With")
+            .allowCredentials(true)
+            .maxAge(3600);
+    }
+
 
 }
